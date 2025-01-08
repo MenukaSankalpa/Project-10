e4b656e2dde6d9fb43c07b7a3ba738c6926adff0
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-08 10:17:24 +0530

    create and update all changes in main.py file

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from difflib import SequenceMatcher
 
-
+with open ('demo 1.txt') as one_file, open('demo 2.txt') as two_file:
+    data_file1 = one_file.read()
+    data_file2 = two_file.read()
+    matches = SequenceMatcher(None, data_file1, data_file2).ratio()
+    print(f"The plagiarized content is {matches}%")
